e5946476bc741ea7acad253d2c869e43fa851570
Aaron Drobek <adrobek09@gmail.com>
2017-08-23 16:06:06 -0400

    initial commit

---
+++ models/pens.js
@@ -0,0 +1,26 @@
+const mongoose    = require("mongoose");
+mongoose.Promise  = require("bluebird");
+const Schema = mongoose.Schema;
+
+mongoose.connect("mongodb://localhost:27017/recipeDirectory");
+
+
+const pensSchema = new Schema({
+  name: {type: String, required: true, unique: true},
+  length: {type: String},
+  color: {type: String},
+  writingType: {type: String}
+  description:[{
+    activation: {type: String},
+    inkColor: {type: String},
+    material: {type: String}
+  }]
+  refillable: {type: Boolean}
+  weight: {type: Number}
+  price: {type: Number}
+});
+
+
+const Pens = mongoose.model("Pens", pensSchema);
+
+module.exports = Pens;
---
+++ routes/index.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const Recipe  = require("../models/index");
+const router  = express.Router();
+
+
+
+module.exports = router;
